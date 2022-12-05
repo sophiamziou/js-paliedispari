21665304af384fa50c133d54b7cafc617b743b0d
sophiamziou <sofiamziou@gmail.com>
2022-12-05 23:02:24 +0100

    exercise pali done

---
+++ js-pali/js/script.js
@@ -0,0 +1,17 @@
+
+let parola = prompt('inserisci parola');
+
+let parolaInvertita = palindroma(parola);
+
+function palindroma(parl){
+    parola2 = parl.split("").reverse().join('');
+    return parola2;
+}
+
+if (parolaInvertita === parola){
+    console.log('la parola è palindroma')
+}
+
+else{
+    console.log('la parola non è palindroma')
+}
---
+++ js-pali/readme.txt
@@ -0,0 +1,5 @@
+Esercizio Palindroma
+
+Elenco passaggi
+1- inseririmento parola
+2- creare la funzione per capire se la parola inserita è palindroma
